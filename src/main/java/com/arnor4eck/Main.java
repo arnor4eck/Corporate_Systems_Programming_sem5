f8b606729f5eff94bef8f95d0fdf2aa83151b23b
Katya <klenovakatusha@gmail.com>
2026-09-02 23:42:52 +0300

    Сделан вывод в консоль вариантов выбора действий пользователя.

--- src/main/java/com/arnor4eck/Main.java
+++ src/main/java/com/arnor4eck/Main.java
@@ -1,7 +1,23 @@
 package com.arnor4eck;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        int num;
+        Scanner in = new Scanner(System.in);
+        do {
+            System.out.print("========= СИСТЕМА УПРАВЛЕНИЯ КЛАДБИЩЕМ =========\n" +
+                    "1. Заявки на место\n" +
+                    "2. Конкретная заявка на место (по id)\n" +
+                    "3. Места на кладбище\n" +
+                    "4. Конкретное место на кладбище (по id)\n" +
+                    "5. Фильтрация по статусу места\n" +
+                    "6. Статистика\n" +
+                    "7. Экспорт данных\n" +
+                    "0. Выход\n" +
+                    "Выберите действие: ");
+            num = in.nextInt();
+        } while (num != 0);
 
     }
 }
