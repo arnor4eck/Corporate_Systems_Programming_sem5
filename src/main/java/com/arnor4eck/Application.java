9bc02eca467e5a0307e8ce7ec58c9a9b9acadedb
arnor4eck <KivalinNik@mail.ru>
2026-09-03 10:59:24 +0300

    Add Application class

---
+++ src/main/java/com/arnor4eck/Application.java
@@ -0,0 +1,8 @@
+package com.arnor4eck;
+
+public final class Application {
+
+    public void run() {
+
+    }
+}
--- src/main/java/com/arnor4eck/Main.java
+++ src/main/java/com/arnor4eck/Main.java
@@ -2,6 +2,7 @@ package com.arnor4eck;
 
 public class Main {
     public static void main(String[] args) {
-
+        Application app = new Application();
+        app.run();
     }
 }
