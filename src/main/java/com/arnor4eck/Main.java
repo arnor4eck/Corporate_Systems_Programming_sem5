592542e101fc175f406e0879db87a5358fd788ba
arnor4eck <KivalinNik@mail.ru>
2026-09-04 00:51:03 +0300

    Fixed encoding

--- src/main/java/com/arnor4eck/Main.java
+++ src/main/java/com/arnor4eck/Main.java
@@ -1,9 +1,13 @@
 package com.arnor4eck;
 
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+         System.setOut(new PrintStream(System.out, true, StandardCharsets.UTF_8));
+
         try(Scanner scanner = new Scanner(System.in)) {
             Application app = new Application(scanner);
             app.run();
