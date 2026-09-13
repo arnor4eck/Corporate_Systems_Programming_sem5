57448add8e186832276ed1491631417d141e4df5
arnor4eck <KivalinNik@mail.ru>
2026-09-14 00:32:32 +0300

    Change MenuProvider to abstract class

--- src/main/java/com/arnor4eck/MenuProvider.java
+++ src/main/java/com/arnor4eck/MenuProvider.java
@@ -3,7 +3,7 @@ package com.arnor4eck;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class MenuProvider {
+public abstract class MenuProvider {
 
     private final Scanner scanner;
 
@@ -16,40 +16,7 @@ public class MenuProvider {
         return getNumber();
     }
 
-    private void showMenu() {
-        System.out.print("========= СИСТЕМА УПРАВЛЕНИЯ КЛАДБИЩЕМ =========\n" +
-                "1. Заявки\n" +
-                "2. Места на кладбище\n" +
-                "3. Экспорт данных\n" +
-                "0. Выход\n" +
-                "Выберите действие: ");
-    }
-
-    private void showRequestMenu() {
-        System.out.print("========= ЗАЯВКИ =========\n" +
-                "1. Все заявки\n" +
-                "2. Конкретная заявка\n" +
-                "3. Фильтрация по статусу заявки\n" +
-                "4. Статистика\n" +
-                "Выберите действие: ");
-    }
-
-    private void showPlotMenu() {
-        System.out.print("========= МЕСТА НА КЛАДБИЩЕ =========\n" +
-                "1. Все места\n" +
-                "2. Конкретное место\n" +
-                "3. Фильтрация по статусу места\n" +
-                "4. Фильтрация по сектору\n" +
-                "Выберите действие: ");
-    }
-
-    private void showExportMenu() {
-        System.out.print("========= ЭКСПОРТ ДАННЫХ =========\n" +
-                "1. Общий экспорт\n" +
-                "2. Экспорт заявок\n" +
-                "3. Экспорт мест\n" +
-                "Выберите действие: ");
-    }
+    public abstract void showMenu();
 
     private int getNumber() {
         int num;
