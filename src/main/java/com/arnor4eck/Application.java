646762f11862dd18a9c22a6874088fded633b2d4
arnor4eck <KivalinNik@mail.ru>
2026-09-14 00:50:02 +0300

    fix exit condition

--- src/main/java/com/arnor4eck/Application.java
+++ src/main/java/com/arnor4eck/Application.java
@@ -7,11 +7,19 @@ public final class Application {
 
     private final MenuProvider menu;
 
+    private static final String EXIT = "Выход";
+    private static final List<String> MENU_UNITS = List.of("Заявки", "Места на кладбище", "Экспорт данных", EXIT);
+    private static final int EXIT_CONDITION;
+
+    static {
+        EXIT_CONDITION = MENU_UNITS.indexOf(EXIT) + 1;
+    }
+
     public Application(Scanner scanner) {
         this.menu = new MenuProvider(
                 "========= СИСТЕМА УПРАВЛЕНИЯ КЛАДБИЩЕМ =========",
                 scanner,
-                List.of("Заявки", "Места на кладбище", "Экспорт данных", "Выход")
+                MENU_UNITS
         );
     }
 
@@ -25,6 +33,6 @@ public final class Application {
     }
 
     private boolean shouldBeExit(int num) {
-        return num == 0;
+        return num == EXIT_CONDITION;
     }
 }
