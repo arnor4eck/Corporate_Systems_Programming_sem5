e1a4a20fe5408397433d20d663dd58cc234a61e0
Sashurman <top.tank.tanki.online@gmail.com>
2026-09-14 10:15:31 +0300

    feat:customerRepository

---
+++ src/main/java/com/arnor4eck/repository/CustomerRepository.java
@@ -0,0 +1,108 @@
+package com.arnor4eck.repository;
+
+import com.arnor4eck.model.Customer;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerRepository extends AbstractJDBCIRepository<Customer> {
+    public CustomerRepository(DataSource dataSource) {
+        super(dataSource);
+    }
+
+    @Override
+    protected Customer mapRow(ResultSet rs) throws SQLException {
+        return new Customer(
+                rs.getInt("id"),
+                rs.getString("full_name"),
+                rs.getString("phone"),
+                rs.getString("email"),
+                rs.getObject("created_at", LocalDateTime.class)
+        );
+    }
+    @Override
+    public Optional<Customer> get(int id) {
+        String sql = "SELECT id, full_name, phone," +
+                " email, created_at" +
+                " FROM customers WHERE id = ?";
+        try (Connection conn = this.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            try (ResultSet rs = stmt.executeQuery()){
+                if (rs.next()) {
+                    return Optional.empty();
+                }
+                return Optional.of(mapRow(rs));
+            }
+        }
+        catch (SQLException e){
+            throw new RuntimeException(String.format(
+                    "SQL exception: %s", e.getMessage()
+            ));
+        }
+    }
+
+    @Override
+    public Collection<Customer> getAll() {
+        String sql = "SELECT id, full_name, phone," +
+                "email, created_at" +
+                "FROM customers";
+        List<Customer> list = new ArrayList<>();
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()){
+            while (rs.next()){
+                list.add(mapRow(rs));
+            }
+        }
+        catch (SQLException e){
+            throw new RuntimeException(String.format(
+                    "SQL exception: %s", e.getMessage()
+            ));
+        }
+        return list;
+    }
+
+    @Override
+    public void save(Customer value) {
+        String sql =
+                "INSERT INTO customers(full_name, phone, email, created_at)" +
+                        " VALUES(?, ?, ?, ?);";
+
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setString(1, value.fullName());
+            stmt.setString(2, value.phone());
+            stmt.setString(3, value.email());
+            stmt.setTimestamp(4, Timestamp.valueOf(value.createdAt()));
+            stmt.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(String.format(
+                    "SQL exception: %s", e.getMessage()
+            ));
+        }
+    }
+    @Override
+    public void delete(int id) {
+        String sql = "DELETE FROM customers WHERE id = ?";
+        try (Connection conn = getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+            stmt.setInt(1, id);
+            stmt.executeUpdate();
+        }
+        catch (SQLException e){
+            throw new RuntimeException(String.format(
+                    "SQL exception: %s", e.getMessage()
+            ));
+        }
+
+    }
+
+
+}
