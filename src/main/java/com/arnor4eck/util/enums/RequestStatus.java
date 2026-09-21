a7a35a8fd50f3ddc3c711ba4b9d511eaf8ad67b6
arnor4eck <KivalinNik@mail.ru>
2026-09-21 16:48:12 +0300

    add value

--- src/main/java/com/arnor4eck/util/enums/RequestStatus.java
+++ src/main/java/com/arnor4eck/util/enums/RequestStatus.java
@@ -1,11 +1,22 @@
 package com.arnor4eck.util.enums;
 
 public enum RequestStatus {
-    NEW,
-    PROCESSING,
-    APPROVED,
-    REJECTED,
-    COMPLETED;
+    NEW("Новый"),
+    PROCESSING("В обработке"),
+    APPROVED("Одобренный"),
+    REJECTED("Отказанный"),
+    COMPLETED("Выполненный");
+
+    private final String value;
+
+    RequestStatus(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
     public static RequestStatus fromString(String str){
         if (str == null) {
             throw new IllegalArgumentException("Error with parse RequestStatus");
@@ -17,4 +28,6 @@ public enum RequestStatus {
         }
         throw new IllegalArgumentException("Error with parse RequestStatus");
     }
+
+
 }
